afc74ee3df366b6e0c52259284c64c5ec3c5fc8c
gyang <Gavin.Yang@acxiom.com>
2018-06-15 22:00:24 +0800

    26

---
+++ 26/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	removeDuplicates(nums)
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	fmt.Println(nums)
+	return i + 1
+}
